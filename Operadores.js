7482e908d584bdc004c468bf09a1786f37efafc0
Joshua Dominguez <joshuajuliandominguez@gmail.com>
2023-11-20 15:37:13 -0500

    Commit Operadores

---
+++ Operadores.js
@@ -0,0 +1,23 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Adicion y strings
+console.log("Hola " + "Camilo");
+    var nombre = "Camilo";
+    var saludo = "Hola " + nombre;
+console.log(saludo)
