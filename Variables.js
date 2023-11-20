54e2217bd4d6516c652e2a831d5c1ee104041184
Joshua Dominguez <joshuajuliandominguez@gmail.com>
2023-11-20 15:09:41 -0500

    Commit variables

---
+++ Variables.js
@@ -0,0 +1,26 @@
+//string
+
+var bootcamp = "Henry";
+
+//Numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 10.75;      
+
+//Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1; 
+console.log (cajon1);
+
+//Null
+
+var sinNada = null;
